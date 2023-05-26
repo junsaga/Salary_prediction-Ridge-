7e6d3eab6080c4358d6768fadea667eeebf48cc0
junsaga <124113134+junsaga@users.noreply.github.com>
2023-05-26 11:55:42 +0900

    수정사항

--- app_home.py
+++ app_home.py
@@ -6,7 +6,7 @@ import seaborn as sns
 
 def run_app_home():
 
-    st.subheader('Welcome~~')
+    st.subheader('안녕하세요')
 
     st.text('이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용입니다')
     st.text('데이터 분석도 가능하고, 고객 정보를 넣으면 구매 차량 가격도 예측합니다')
